8c86e104574b5e991beccd7a95af3df61b4d046c
despinoza17 <despi17@vt.edu>
2022-10-22 22:30:10 -0400

    Added header component

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,4 +1,13 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  solid,
+  regular,
+  brands,
+  icon,
+} from "@fortawesome/fontawesome-svg-core/import.macro";
+
+import "../css/Header.css";
 
 export default class Header extends React.Component {
   constructor(props) {
@@ -7,8 +16,47 @@ export default class Header extends React.Component {
   }
   render() {
     return (
-      <div className="text-3xl w-100 h-32 bg-light-gray">
-        {/* Create header component */}header component
+      <div className="w-full h-32 bg-light-gray ">
+        <div className="mx-2 h-full relative items-center flex">
+          <div className="absolute top-0 right-0 pt-2">
+            <span className="inline-block align-middle">
+              <FontAwesomeIcon icon={regular("circle-user")} className="h-6" />
+            </span>
+            <span className="font-inter text-sm pl-1 inline-block align-middle">
+              Sign in or Register
+            </span>
+          </div>
+          <div className="flex w-full">
+            <div className="flex-1 font-carter-one text-3xl text-left">
+              <span className="text-steel-blue inline-block align-middle">
+                Reward
+              </span>
+              <span className="text-amazon inline-block align-middle">
+                Ready
+              </span>
+            </div>
+            <div className="flex-0">
+              <div className="relative">
+                <input
+                  type="search"
+                  className="h-9 rounded-xl font-inter text-sm pl-2 pr-8 w-72"
+                  placeholder="Search for deals, coupons, and merchants"
+                />
+                <button className="absolute right-3 bottom-2">
+                  <FontAwesomeIcon icon={solid("magnifying-glass")} />
+                </button>
+              </div>
+            </div>
+            <div className="flex-1 text-left">
+              <span className="inline-block align-middle pl-3">
+                <FontAwesomeIcon icon={solid("map-location")} />
+              </span>
+              <span className="pl-2 inline-block align-middle text-amazon font-semibold font-sm font-inter">
+                <a className="inline-block align-middle">Enter Location</a>
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "oxford-blue": "#01163b",
+        "light-gray": "#cdcdcd",
+        "brunswick-green": "#214e34",
+        "steel-blue": "#3581b8",
+        honeydew: "#dff8eb",
+        "shamrock-green": "#21975a",
+        amazon: "#36b156",
+      },
+      fontFamily: {
+        inter: ["Inter"],
+        "carter-one": ["Carter One"],
+      },
+    },
   },
   plugins: [],
 };
