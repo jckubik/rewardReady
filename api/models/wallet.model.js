ccf37ce364211c7577c327c35d4b3a3f8e1b8aa9
Rohit Rane <rohitrane@vt.edu>
2022-10-30 15:39:30 -0400

    History

--- api/models/wallet.model.js
+++ api/models/wallet.model.js
@@ -1,5 +1,6 @@
 module.exports = (sequelize, Sequelize) => {
     return sequelize.define('wallet', {
+        items: {type: Sequelize.JSON, allowNull: false},
         history: {type: Sequelize.JSON, allowNull: false}
     });
 };
--- api/routes/wallet.routes.js
+++ api/routes/wallet.routes.js
@@ -4,41 +4,22 @@ const walletController = require("../controllers/wallet.controller");
 const userMiddleware = require("../middleware/user.middleware");
 const walletMiddleware = require("../middleware/wallet.middleware");
 
-module.exports = (app) => {
-  router.get(
-    "/items/cards",
-    userMiddleware.verifyToken,
-    walletController.fetchCards
-  );
-  router.post(
-    "/items/cards/insert",
-    [
-      userMiddleware.verifyToken,
-      walletMiddleware.checkCardValidity,
-      walletMiddleware.checkCardDuplicates,
-    ],
-    walletController.insertCard
-  );
-  router.post(
-    "/items/cards/remove",
-    [userMiddleware.verifyToken, walletMiddleware.checkCardValidity],
-    walletController.removeCard
-  );
-  router.get(
-    "/recommend/card",
-    userMiddleware.verifyToken,
-    walletController.recommendCard
-  );
-  router.get(
-    "/history",
-    userMiddleware.verifyToken,
-    walletController.fetchHistory
-  );
-  router.post(
-    "/history/insert",
-    userMiddleware.verifyToken,
-    walletController.insertHistory
-  );
+module.exports = app => {
+    router.get('/items', userMiddleware.verifyToken, walletController.fetchItems);
+    router.get('/items/cards', userMiddleware.verifyToken, walletController.fetchCards);
+    router.post(
+        '/items/cards/insert',
+        [userMiddleware.verifyToken, walletMiddleware.checkCardValidity, walletMiddleware.checkCardDuplicates],
+        walletController.insertCard
+    );
+    router.post(
+        '/items/cards/remove',
+        [userMiddleware.verifyToken, walletMiddleware.checkCardValidity],
+        walletController.removeCard
+    );
+    router.get('/recommend/card', userMiddleware.verifyToken, walletController.recommendCard);
+    router.get('/history',  userMiddleware.verifyToken, walletController.fetchHistory);
+    router.post('/history/insert', userMiddleware.verifyToken, walletController.insertHistory);
 
   app.use("/api/wallet", router);
 };
