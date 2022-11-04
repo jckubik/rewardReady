b9b8c8974f05f6b75ff699d03b42962495b1e422
jackp96 <jackp96@vt.edu>
2022-11-03 22:07:36 -0400

    Coupon functionality in progress

--- api/config/api.config.js
+++ api/config/api.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    ccStackSecret: 'bd7018de35mshc04835b79363b6ep17d276jsn603bdc0aceaf'
+    ccStackSecret: 'bd7018de35mshc04835b79363b6ep17d276jsn603bdc0aceaf',
+    discountSecret: 'YxhRTxQe'
 };
---
+++ client/src/components/FindCoupon.js
@@ -0,0 +1,70 @@
+import { useEffect, useState } from 'react';
+import { discountSecret } from '../utils/api';
+
+const FindCoupon = () => {
+    const [boolCoupons, setBoolCoupons] = useState();
+    const [coupons, setCoupons] = useState([]);
+    const [couponToVisit, setCouponToVisit] = useState("");
+
+
+    async function fetchCoupons() {
+        const options = {
+            method: 'GET',
+            url: "https://api.discountapi.com/v2/deals",
+            headers: {
+                'Access-Control-Allow-Origin': "https://api.discountapi.com/v2/deals",
+                key: discountSecret 
+            }
+        };
+  
+
+        await fetch(`https://api.discountapi.com/v2/deals?api_key=${discountSecret}`, options)
+            .then(response => response.json)
+            .then(response => setCoupons(response.results))
+            .catch(err => console.error(err));
+    }
+
+    useEffect(() => {
+        if (boolCoupons) {
+            fetchCoupons();
+        }
+    })
+
+    return (
+        <div className='container flex-1'>
+            <div className='flex-1'>
+                <label htmlFor='boolCoupon'>Do you want coupons?</label>
+
+                <select name='boolCoupon' id='boolCoupon' onChange={(e) => setBoolCoupons([e.target.value])}>
+                    <option key={0} value='empty'>Select</option>
+                    <option key={1} value='true'>Yes</option>
+                </select>
+            </div>
+            {
+            coupons.length === 0 ? 
+                ""
+                :
+
+            <div className='flex-1'>
+                <div className='flex-1'>
+                    <label htmlFor='coupons'>These coupons are available</label>
+                    <select 
+                        name="coupons" 
+                        id="coupons" 
+                        onChange={(event) => setCouponToVisit(coupons[event.target.value])}
+                    >
+                    <option value="empty">Select</option>
+                    {
+                        coupons.map((coupon, index) => (
+                        <option value={index} key={coupon.id}>{ coupon.title }</option>
+                        ))
+                    }
+                    </select>
+                </div>
+            </div>
+            }
+        </div>
+    );
+}
+
+export default FindCoupon;
