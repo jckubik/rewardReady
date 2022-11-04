7a51336215000ae6cb80ba77f23b72d4eb662002
jackp96 <jackp96@vt.edu>
2022-11-03 22:07:36 -0400

    Coupon functionality in progress

---
+++ client/src/components/recommendDisplay.js
@@ -0,0 +1,34 @@
+import api from '../utils/api';
+import { useState } from 'react';
+
+const RecommendDisplay = (props) => {
+
+    const [recommendation, setRecommendation] = useState();
+    
+    // May need to change depending on how checking for logged in user works
+    if (!localStorage.getItem('token')) {
+        return (
+            <div>
+                <h1>Please sign in to view credit card recommendation</h1>
+            </div>
+        );
+    } else {
+        
+        // var store;
+
+        // function getStore() {
+        //     //TBD needs state from locateme - should move it up from locateme to app with call in header as well
+        //     return store;
+        // }
+
+        setRecommendation(api.getCardRecommendationsForUser(props.store));
+        
+        return (
+            <div>
+                <h1>Card Recommendation: {recommendation}</h1>
+            </div>
+        ); 
+    }
+}
+
+export default RecommendDisplay;
