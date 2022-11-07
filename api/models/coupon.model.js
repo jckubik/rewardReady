04bc3e02ea1a9cad2226b8880f0487ced982ea27
jackp96 <jackp96@vt.edu>
2022-11-07 16:21:40 -0500

    Added coupon backend

--- api/models/coupon.model.js
+++ api/models/coupon.model.js
@@ -1,12 +1,15 @@
 module.exports = (sequelize, Sequelize) => {
     return sequelize.define('coupon', {
-        couponId: {type: Sequelize.INTEGER, allowNull: false, unique: true},
-        title: {type: Sequelize.STRING, allowNull: false},
-        description: {type: Sequelize.STRING},
-        price: {type: Sequelize.FLOAT, allowNull: false},
-        value: {type: Sequelize.FLOAT, allowNull: false},
-        imageUrl: {type: Sequelize.STRING},
-        createdAt: {type: Sequelize.DATE},
-        merchantName: {type: Sequelize.STRING, allowNull: false}
+        couponId: {type: Sequelize.INTEGER, allowNull: false, unique: true}, //json.links.link.link-id
+        title: {type: Sequelize.STRING, allowNull: false}, //json.links.link.link-name
+        description: {type: Sequelize.STRING}, //json.links.link.description
+        // price: {type: Sequelize.FLOAT, allowNull: false},
+        // value: {type: Sequelize.FLOAT, allowNull: false},
+        // imageUrl: {type: Sequelize.STRING},
+        // createdAt: {type: Sequelize.DATE},
+        merchantName: {type: Sequelize.STRING, allowNull: false}, //json.links.link.advertiser-name
+        linkCodeHtml: {type: Sequelize.STRING}, //json.links.link.link-code-html
+        clickUrl: {type: Sequelize.STRING, allowNull: false}, // json.links.link.clickUrl
+        couponCode: {type: Sequelize.STRING, allowNull: false}, // json.links.link.coupon-code
     })
 }
