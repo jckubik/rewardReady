7506ea767e3dcf1ecc468c9e70966ea35919a017
jackp96 <jackp96@vt.edu>
2022-11-06 21:45:14 -0500

    Fixed frontend for card recommendation

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
@@ -8,6 +9,23 @@ import Cards from "./pages/Cards";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 // import AddCard from "./components/AddCard";
+=======
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import './App.css';
+// import LocateMe from "./Components/LocateMe";
+import Home from './pages/Home';
+import Setting from './pages/Setting';
+import Personal from './pages/Setting/Personal';
+import Cards from './pages/Setting/Cards';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import AddCard from './components/AddCard';
+import FindCoupon from './components/FindCoupon';
+import RecommendDisplay from './components/RecommendDisplay';
+import LocateMe from './components/LocateMe';
+>>>>>>> 941c318 (Fixed frontend for card recommendation)
 
 import session from "./context/user";
 
@@ -18,6 +36,10 @@ function App() {
   const changeState = (storeData) => {
     setStore(storeData);
   };
+
+  const changeState = (storeData) => {
+    setStore(storeData)
+  }
 
   return (
     <session.Provider
@@ -32,10 +54,22 @@ function App() {
           <Route index element={<Home />} />
           <Route path="personal" element={<Personal />} />
           <Route path="cards" element={<Cards />} />
+<<<<<<< HEAD
         </Routes>
         <Footer />
       </div>
     </session.Provider>
+=======
+        </Route>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <LocateMe data={store.data} changeState={changeState} />
+      {/* <AddCard /> */}
+      {/* <FindCoupon /> */}
+      <RecommendDisplay store={store.data}/>
+      <Footer />
+    </div>
+>>>>>>> 941c318 (Fixed frontend for card recommendation)
   );
 }
 
