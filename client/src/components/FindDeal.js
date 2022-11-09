761f657260d62ef971082bbd507ea23f29431653
jackp96 <jackp96@vt.edu>
2022-11-08 20:22:11 -0500

    Refactored layout code

--- client/src/components/FindDeal.js
+++ client/src/components/FindDeal.js
@@ -22,7 +22,6 @@ const FindDeal = () => {
     try {
       let deal = await api.getRandomDeal();
 
-<<<<<<< HEAD
       setDeals({
         dealId: deal["couponId"],
         title: deal["title"],
@@ -41,34 +40,6 @@ const FindDeal = () => {
       return deals;
     } catch (err) {
       console.log(err);
-=======
-    async function fetchDeals() {
-        const options = {
-            method: 'GET',
-        };
-  
-        try {
-            let deal = await fetch(`http://localhost:9000/api/deal/random`, options)
-            const dealJson = deal.json();
-            dealJson.then((obj) => setDeals({
-                dealId: obj["couponId"],
-                title: obj["title"],
-                merchantName: obj["merchantName"],
-                clickUrl: obj["clickUrl"],
-                couponCode: obj["couponCode"]
-            }))
-            dealJson.then((obj) => props.changeDeal({
-                title: obj["title"],
-                subtitle: obj["merchantName"],
-                imgSrc: obj["imageUrl"]
-            }));
-            dealJson.then((obj) => console.log(obj["imageUrl"]))
-            setBoolDeals(false);
-            return deals;
-        } catch (err) {
-            console.log(err);
-        }
->>>>>>> f2764cd (Fixed missing image for deals)
     }
   }
 
@@ -82,29 +53,7 @@ const FindDeal = () => {
     boolFetch();
   });
 
-<<<<<<< HEAD
   return dealReturn;
 };
-=======
-    return (
-        <div hidden>
-            {/* <img
-            className="object-cover w-60 h-36"
-            alt={this.props.item.title}
-            src={this.props.item.imgSrc}
-            /> */}
-            <div className="pt-4">
-                <p className="text-inter font-extrabold text-xl text-oxford-blue uppercase">
-                    {deals.title}
-                </p>
-                <p className="text-inter font-bold text-base text-brunswick-green">
-                    {deals.merchantName}
-                </p>
-            </div>
-        </div>
-        // <div className='container flex-1'>
-        //     <div className='flex-1'>
-        //         <label htmlFor='boolCoupon'>Do you want coupons?</label>
->>>>>>> f2764cd (Fixed missing image for deals)
 
 export default FindDeal;
