0eefe1fabad387e22b7542be4aa2e0f183f03675
jackp96 <jackp96@vt.edu>
2022-11-11 10:37:26 -0500

    Added insert support for categories

--- api/routes/category.routes.js
+++ api/routes/category.routes.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const categoryController = require("../controllers/category.controller");
 
 module.exports = (app) => {
-    // router.post("/insert", categoryController.insertCategory);
+    router.post("/insert", categoryController.insertCategory);
 
     app.use("/api/category", router);
 };
