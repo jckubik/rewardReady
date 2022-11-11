3583edaa0aee4b8f203015e4dd0d504576347ff4
jackp96 <jackp96@vt.edu>
2022-11-11 10:37:26 -0500

    Added insert support for categories

--- api/controllers/category.controller.js
+++ api/controllers/category.controller.js
@@ -3,11 +3,11 @@ const db = require('../models');
 const Category = db.categories;
 const Op = db.sequelize.Op;
 
-// exports.insertCategory = async (req, res) => {
-//     const body = req.body;
-//     return Category.create({
-//         categoryId: body.categoryId
-//     })
-//     .then(() => res.end())
-//     .catch(() => res.status(501).send({message: 'Unexpected error'}));
-// }
+exports.insertCategory = async (req, res) => {
+    const body = req.body;
+    return Category.create({
+        categoryId: body.categoryId
+    })
+    .then(() => res.end())
+    .catch(() => res.status(501).send({message: 'Unexpected error'}));
+}
