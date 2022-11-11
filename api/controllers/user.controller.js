1b2e3f04f6687aa69714f5f1fdd73e3b345106c5
Jacob <jacob@jacobs-mbp.lan>
2022-11-11 15:13:12 -0500

    104: completed user.controller - updatePassword, requestPasswordReset, resetPassword

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -12,7 +12,7 @@ const Wallet = db.wallets;
 const Op = db.Sequelize.Op;
 const tempUtil = require("../utils/temp.util");
 
-const clientLink = "http://localhost:9000/";
+const clientLink = "http://localhost:3000/";
 
 exports.register = async (req, res) => {
   const { body } = req;
@@ -160,6 +160,137 @@ exports.updateInfo = async (req, res, next) => {
 // Update user password
 exports.updatePassword = async (req, res, next) => {
   const { userId } = req;
+  const { oldPassword, newPassword } = req.body;
+
+  if (!userId) {
+    res.status(400).send({ message: "UserId not found." });
+    return;
+  }
+
+  // Error if full information not sent
+  if (
+    newPassword === null ||
+    newPassword === "" ||
+    oldPassword === null ||
+    oldPassword === ""
+  ) {
+    res.status(400).send({ message: "Content cannot be empty." });
+    return;
+  }
+
+  try {
+    // find the proper user to update
+    const user = await User.findOne({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (user) {
+      // Verify that the oldPassword matches the current password set for the user
+      const oldPasswordAuth = bcrypt.compareSync(oldPassword, user.password);
+      if (!oldPasswordAuth) {
+        res
+          .status(400)
+          .send({
+            message:
+              "Unauthorized - oldPassword for the given user does not match the password on record.",
+          });
+        return;
+      }
+
+      // Salt and hash the password
+      const hash = bcrypt
+        .genSalt(10)
+        .then((salt) => bcrypt.hash(newPassword, salt))
+        .then((hash) => ({
+          password: hash,
+        }));
+
+      // Set and save the data to the database
+      hash.then((pw) => user.set(pw));
+      hash.then(() => user.save());
+
+      res
+        .status(200)
+        .send({
+          message:
+            "User password updated successfully in DB. Trying to send confirmation email now.",
+        });
+
+      // Send confirmation email to user
+      req.body = {
+        email: user.email,
+        firstName: user.firstName,
+      };
+      next();
+    } else {
+      res.status(404).send({ message: "User requested does not exist." });
+    }
+  } catch (error) {
+    console.log(error);
+    res
+      .status(400)
+      .send({
+        message: `Unexpected error while trying to update the user password. - ${error}`,
+      });
+  }
+};
+
+// Generate password reset request - sends email to user
+exports.requestPasswordReset = async (req, res, next) => {
+  const { email } = req.body;
+
+  try {
+    // Grab user matching the given email
+    let user = await User.findOne({
+      where: {
+        email: email,
+      },
+    });
+
+    // Check to see if a user with request email exists
+    if (user === null) {
+      res
+        .status(404)
+        .send({ message: "User with the given email does not exist." });
+      next();
+    }
+
+    // Make the user a plain object for jewt.sign()
+    user = user.get({ plain: true });
+
+    // Generate resetToken
+    const resetToken = jwt.sign(user, authConfig.secret, {
+      expiresIn: "1800s",
+    });
+
+    // Generate needed email variables
+    const resetLink = `${clientLink}/passwordReset?token=${resetToken}&id=${user.id}`;
+    const expirationDate = Date.now() + 1800000;
+
+    // Send reset email to user
+    // await sendPasswordResetEmail(email, user.firstName, resetLink, expirationDate);
+    req.body = {
+      email: email,
+      firstName: user.firstName,
+      resetLink: resetLink,
+      expirationDate: expirationDate,
+    };
+    next();
+  } catch (error) {
+    console.log(error);
+    res
+      .status(400)
+      .send({
+        message: `Unexpected error while trying to update the user password. - ${error}`,
+      });
+  }
+};
+
+// Reset user password
+exports.resetPassword = async (req, res, next) => {
+  const { userId } = req;
   const { newPassword } = req.body;
 
   if (!userId) {
@@ -169,7 +300,7 @@ exports.updatePassword = async (req, res, next) => {
 
   // Error if full information not sent
   if (newPassword === null || newPassword === "") {
-    res.status(400).send({ message: "newPassword cannot be empty." });
+    res.status(400).send({ message: "Content cannot be empty." });
     return;
   }
 
@@ -183,7 +314,6 @@ exports.updatePassword = async (req, res, next) => {
 
     if (user) {
       // Salt and hash the password
-
       const hash = bcrypt
         .genSalt(10)
         .then((salt) => bcrypt.hash(newPassword, salt))
@@ -191,92 +321,31 @@ exports.updatePassword = async (req, res, next) => {
           password: hash,
         }));
 
+      // Set and save the data to the database
       hash.then((pw) => user.set(pw));
-      // Save the data to the database
       hash.then(() => user.save());
 
-      // Send confirmation email
-      await sendPasswordResetEmail(user.email, user.firstName);
-      console.log(user);
-
-      res.status(200).send({
-        message: "User password updated successfully.",
-        password: user.password,
-      });
+      // Send confirmation email to user
+      req.body = {
+        email: user.email,
+        firstName: user.firstName,
+      };
+      res
+        .status(200)
+        .send({
+          message:
+            "User password reset successfully in DB. Trying to send confirmation email now.",
+        });
       next();
     } else {
       res.status(404).send({ message: "User requested does not exist." });
     }
   } catch (error) {
     console.log(error);
-    res.status(400).send({
-      message: `Unexpected error while trying to update the user password. - ${error}`,
-    });
-  }
-};
-
-exports.requestPasswordReset = async (req, res, next) => {
-  const email = req.body.email;
-  const { userId } = req;
-
-  try {
-    let resetToken;
-    let userForToken;
-    let userTemp;
-    const user = await User.findOne({
-      where: {
-        email: email,
-      },
-    })
-      // .then((user) => (user ? user.get({ plain: true }) : null))
-      .then((user) => (userTemp = user))
-      // .then((user) => console.log(user))
-      .then((authentication) => {
-        const valid = authentication[1];
-        if (valid) {
-          userForToken = authentication[0];
-          resetToken = jwt.sign(userTemp, authConfig.secret, {
-            expiresIn: "1800s",
-          });
-        }
-      });
-
-    // console.log(userForToken);
-
-    // Check to see if a user with request email exists
-    if (userTemp === null) {
-      res
-        .status(404)
-        .send({ message: "User with the given email does not exist." });
-      next();
-    }
-
-    // Generate resetToken
-    // const resetToken = jwt.sign(user, authConfig.secret, { expiresIn: '1800s' });
-    // console.log(typeof resetToken);
-    // Generate needed email variables
-    const resetLink = `${clientLink}/passwordReset?token=${resetToken}&id=${userTemp.id}`;
-    const expirationDate = Date.now() + 1800000;
-    // console.log(typeof resetLink);
-    // Send reset email to user
-    console.log(userTemp);
-    await sendPasswordResetEmail(
-      email,
-      userTemp.firstName,
-      resetLink,
-      expirationDate
-    );
-
-    res
-      .status(200)
-      .send({ message: `Password reset email sent. Reset link: ${resetLink}` });
-    // return resetLink;
-  } catch (error) {
-    console.log(error);
     res
       .status(400)
       .send({
-        message: `Unexpected error while trying to update the user password. - ${error}`,
+        message: `Unexpected error while trying to reset the user password. - ${error}`,
       });
   }
 };
