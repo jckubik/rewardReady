ae60269cf6a37e5eba8e56719db8129daad934e1
anuragsaroha <anurag.srh@gmail.com>
2022-11-27 17:57:41 -0500

    Refactored Change Password, Implmented carousel and fixed layout

--- client/src/components/ShowcaseHelper.js
+++ client/src/components/ShowcaseHelper.js
@@ -10,41 +10,41 @@ import "swiper/css/scrollbar";
 import "../css/ShowcaseHelper.css";
 
 const ShowcaseHelper = (props) => {
-  const elems = [];
+    const elems = [];
 
-  for (let i = 0; i < 10; i++) {
-    if (props.type === "deal") {
-      elems.push(FindDeal());
-    } else {
-      elems.push(FindCoupon());
+    for (let i = 0; i < 4; i++) {
+        if (props.type === "deal") {
+            elems.push(FindDeal());
+        } else {
+            elems.push(FindCoupon());
+        }
     }
-  }
-  return (
-    <div className="w-full">
-      <div
-        className={`w-full px-4 py-14 ${
-          props.type === "deal" ? "bg-honeydew" : ""
-        }`}
-      >
-        <span className="heading-1 uppercase">{props.title}</span>
-        <div className="flex px-8 pt-10 justify-between gap-5 w-full">
-          <Swiper
-            className="swiper-container"
-            slidesPerView={4}
-            spaceBetween={50}
-            modules={[Navigation]}
-            navigation
-          >
-            {elems.map((item, index) => (
-              <SwiperSlide key={index}>
-                <ShowcaseItem item={item} showImage={true} />
-              </SwiperSlide>
-            ))}
-          </Swiper>
+    return (
+        <div className="w-full">
+            <div className={`w-full px-4 py-14 ${props.backgroundColor}`}>
+                <span className="heading-1 uppercase">{props.title}</span>
+                <div className="flex px-8 pt-10 justify-between gap-5 w-full">
+                    <Swiper
+                        slidesPerView={3}
+                        spaceBetween={50}
+                        modules={[Navigation]}
+                        navigation
+                    >
+                        {elems.map((item, index) => (
+                            <SwiperSlide key={index}>
+                                <ShowcaseItem
+                                    item={item}
+                                    showImage={
+                                        props.type == "coupon" ? false : true
+                                    }
+                                />
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ShowcaseHelper;
