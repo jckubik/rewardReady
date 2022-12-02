fefc3bd7e64c3ab1f8325dfa3ed54d07ac0a3683
despinoza17 <despi17@vt.edu>
2022-12-02 00:47:52 -0500

    88: Arrows no longer overlap swiper items

--- client/src/components/ShowcaseHelper.js
+++ client/src/components/ShowcaseHelper.js
@@ -7,41 +7,40 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/scrollbar";
+import "../css/ShowcaseHelper.css";
 
 const ShowcaseHelper = (props) => {
-    const elems = [];
+  const elems = [];
 
-    for (let i = 0; i < 4; i++) {
-        if (props.type === "deal") {
-            elems.push(FindDeal());
-        } else {
-            elems.push(FindCoupon());
-        }
+  for (let i = 0; i < 4; i++) {
+    if (props.type === "deal") {
+      elems.push(FindDeal());
+    } else {
+      elems.push(FindCoupon());
     }
-    return (
-        <div className="w-full">
-            <div className={`w-full px-4 py-14 ${props.backgroundColor}`}>
-                <span className="heading-1 uppercase">{props.title}</span>
-                <div className="flex px-8 pt-10 justify-between gap-5 w-full">
-                    <Swiper
-                        slidesPerView={3}
-                        spaceBetween={50}
-                        modules={[Navigation]}
-                        navigation
-                    >
-                        {elems.map((item, index) => (
-                            <SwiperSlide key={index}>
-                                <ShowcaseItem
-                                    item={item}
-                                    showImage={ true }
-                                />
-                            </SwiperSlide>
-                        ))}
-                    </Swiper>
-                </div>
-            </div>
+  }
+  return (
+    <div className="w-full">
+      <div className={`w-full px-4 py-14 ${props.backgroundColor}`}>
+        <span className="heading-1 uppercase">{props.title}</span>
+        <div className="flex px-8 pt-10 justify-between gap-5 w-full">
+          <Swiper
+            className="swiper-container"
+            slidesPerView={3}
+            spaceBetween={50}
+            modules={[Navigation]}
+            navigation
+          >
+            {elems.map((item, index) => (
+              <SwiperSlide key={index}>
+                <ShowcaseItem item={item} showImage={true} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ShowcaseHelper;
