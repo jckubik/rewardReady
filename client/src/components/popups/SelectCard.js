b758e494975bde44f2f0416f6817813184aa4dc2
Jacob <jacob@jacobs-mbp.lan>
2022-12-03 10:47:43 -0500

    59-added loading screen for add card in wallet

--- client/src/components/popups/SelectCard.js
+++ client/src/components/popups/SelectCard.js
@@ -1,19 +1,23 @@
 import { useEffect, useState } from "react";
 import { addCard, insertCardInToWallet } from "../../reduxSlices/walletSlice";
 import { useDispatch } from "react-redux";
+import "../../css/SelectCard.css";
 import api from "../../utils/api";
 const SelectCard = ({ visible, setVisible }) => {
   const [creditCards, setCreditCards] = useState([]);
   const [selectedCC, setSelectedCC] = useState("");
+  const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
 
   const onSubmit = async () => {
+    setLoading(true);
     let resp = await api.insertCardToWallet(selectedCC);
     if (resp.name && resp.name === "AxiosError") {
       console.log(resp.response.data.message);
     } else {
       dispatch(insertCardInToWallet(resp));
     }
+    setLoading(false);
     setVisible(false);
   };
 
@@ -31,29 +35,34 @@ const SelectCard = ({ visible, setVisible }) => {
       className="w-screen h-screen fixed bg-[#0000009d] top-0 z-50 grid place-items-center font-inter"
       onClick={() => setVisible(false)}
     >
-      <div
-        className="bg-[#e4e7ea] flex flex-col gap-5 text-center py-10 px-10 min-w-[500px] rounded-lg"
-        onClick={(e) => e.stopPropagation()}
-      >
-        <h2 className="text-3xl font-bold">Add Credit Card</h2>
-        <div className="space-x-3">
-          <select
-            defaultValue=""
-            onChange={(e) => handleOnChangeSelect(e)}
-            className="h-9 rounded-xl body w-80"
-          >
-            <option value="">Select credit card</option>
-            {creditCards.map((cc) => (
-              <option key={cc.id} value={cc.id}>
-                {cc.title}
-              </option>
-            ))}
-          </select>
-          <button onClick={onSubmit} className="primary-btn">
-            Add Card
-          </button>
+      {
+        loading ?
+        <div className="loader"></div>
+        :
+        <div
+          className="bg-[#e4e7ea] flex flex-col gap-5 text-center py-10 px-10 min-w-[500px] rounded-lg"
+          onClick={(e) => e.stopPropagation()}
+        >
+          <h2 className="text-3xl font-bold">Add Credit Card</h2>
+          <div className="space-x-3">
+            <select
+              defaultValue=""
+              onChange={(e) => handleOnChangeSelect(e)}
+              className="h-9 rounded-xl body w-80"
+            >
+              <option value="">Select credit card</option>
+              {creditCards.map((cc) => (
+                <option key={cc.id} value={cc.id}>
+                  {cc.title}
+                </option>
+              ))}
+            </select>
+            <button onClick={onSubmit} className="primary-btn">
+              Add Card
+            </button>
+          </div>
         </div>
-      </div>
+      }
     </div>
   ) : null;
 };
