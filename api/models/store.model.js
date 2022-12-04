219d52de57e597ea63d3ee919f6a7a65c00dea66
jackp96 <jackp96@vt.edu>
2022-12-04 13:17:12 -0500

    Change url datatype to TEXT in store model

--- api/models/store.model.js
+++ api/models/store.model.js
@@ -1,7 +1,7 @@
 module.exports = (sequelize, Sequelize) => {
   return sequelize.define("store", {
     name: { type: Sequelize.STRING, allowNull: false, unique: true },
-    webAddress: { type: Sequelize.STRING, allowNull: false },
-    logoAddress: { type: Sequelize.STRING, allowNull: false },
+    webAddress: { type: Sequelize.TEXT, allowNull: false },
+    logoAddress: { type: Sequelize.TEXT, allowNull: false },
   });
 };
